4b719aa27b8792958682ac124dcabbafef014d41
Aamish Rafique <rafique.aamish@gmail.com>
2024-01-18 19:36:46 +0500

    Character Animation

---
+++ Robot/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  function randomDirection() {
+    const directions = ["left", "right", "center"];
+    return directions[Math.floor(Math.random() * directions.length)];
+  }
+
+  function setEyeDirection() {
+    const eyeLeft = document.querySelector(".robot-eye-left");
+    const eyeRight = document.querySelector(".robot-eye-right");
+
+    const directionLeft = randomDirection();
+    const directionRight = randomDirection();
+
+    eyeLeft.style.transformOrigin = `${directionLeft} center`;
+    eyeRight.style.transformOrigin = `${directionRight} center`;
+  }
+
+  setInterval(setEyeDirection, 4000);
+});
