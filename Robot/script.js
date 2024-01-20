0831a25e27fc0c02d4a1c16a39d004118673f799
Aamish Rafique <rafique.aamish@gmail.com>
2024-01-21 01:11:44 +0500

    Update Robot

--- Robot/script.js
+++ Robot/script.js
@@ -22,16 +22,37 @@
 
 document.addEventListener("keydown", function (event) {
   if (event.key === " ") {
-    // You can replace " " with the key you want to trigger the animation
-    toggleEyeAnimation();
+    sleepAwakeState();
   }
 });
 
-function toggleEyeAnimation() {
+function sleepAwakeState() {
   const leftEye = document.querySelector(".robot-eye-left");
   const rightEye = document.querySelector(".robot-eye-right");
+  const innerBody = document.querySelector(".inner");
+  const zzz = document.querySelector(".wake-up");
+  const orb1 = document.querySelector(".orb1");
+  const orb2 = document.querySelector(".orb2");
 
-  // Check if the left eye is in sleep mode, then switch to blink, and vice versa
+  // Check if the Orb is Off, then Switch to On, and Vice Versa
+  if (orb1.classList.contains("off")) {
+    orb1.classList.remove("off");
+    orb1.classList.add("on");
+  } else {
+    orb1.classList.remove("on");
+    orb1.classList.add("off");
+  }
+
+  // Check if the Orb is Off, then Switch to On, and Vice Versa
+  if (orb2.classList.contains("off")) {
+    orb2.classList.remove("off");
+    orb2.classList.add("on");
+  } else {
+    orb2.classList.remove("on");
+    orb2.classList.add("off");
+  }
+
+  // Check if the Left Eye is in Sleep Mode, then Switch to Blink, and Vice Versa
   if (leftEye.classList.contains("animate-sleep")) {
     leftEye.classList.remove("animate-sleep");
     leftEye.classList.remove("zzz");
@@ -41,7 +62,7 @@ function toggleEyeAnimation() {
     leftEye.classList.add("animate-sleep", "zzz");
   }
 
-  // Check if the right eye is in sleep mode, then switch to blink, and vice versa
+  // Check if the Right Eye is in Sleep Mode, then Switch to Blink, and Vice Versa
   if (rightEye.classList.contains("animate-sleep")) {
     rightEye.classList.remove("animate-sleep");
     leftEye.classList.remove("zzz");
@@ -49,5 +70,30 @@ function toggleEyeAnimation() {
   } else {
     rightEye.classList.remove("animate-blink");
     rightEye.classList.add("animate-sleep", "zzz");
+  }
+
+  // Check if the Robot is in Sleep Mode, then Switch to Motion, and Vice Versa
+  if (innerBody.classList.contains("animate-sleep-movement")) {
+    innerBody.classList.remove("animate-sleep-movement");
+    innerBody.classList.add(
+      "animate-wobble",
+      "animate-tilt-head",
+      "animate-breathe",
+      "animate-look"
+    );
+  } else {
+    innerBody.classList.remove(
+      "animate-wobble",
+      "animate-tilt-head",
+      "animate-breathe",
+      "animate-look"
+    );
+    innerBody.classList.add("animate-sleep-movement");
+  }
+
+  if (zzz.classList.contains("snores")) {
+    zzz.classList.remove("snores");
+  } else {
+    zzz.classList.add("snores");
   }
 }
